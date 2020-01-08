3caf23f09cdbf6843d4268eaf5cff11bf000ed4e
JoongSeokD <ljseokd@gmail.com>
2020-01-09 00:56:01 +0900

    플라이웨이트 패턴 구현

---
+++ src/flyweightPattern/Subject.java
@@ -0,0 +1,12 @@
+package flyweightPattern;
+
+public class Subject {
+	private String name;
+
+	public Subject(String name) {
+		super();
+		this.name = name;
+	}
+	
+	
+}
---
+++ src/flyweightPattern/TestPattern.java
@@ -0,0 +1,12 @@
+package flyweightPattern;
+
+public class TestPattern {
+	public static void main(String[] args) {
+		
+		FlyweightFactory flyweight = new FlyweightFactory();
+		flyweight.getSubject("a");
+		flyweight.getSubject("a");
+		flyweight.getSubject("b");
+		flyweight.getSubject("b");
+	}
+}
